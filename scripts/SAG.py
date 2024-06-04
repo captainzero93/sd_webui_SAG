2d4d179b87aae93a8ea5a04eb2ae8b2f64859cd7
AG-w <44581470+AG-w@users.noreply.github.com>
2024-06-04 18:47:04 +0800

    Update SAG.py
    result looks a little better

--- scripts/SAG.py
+++ scripts/SAG.py
@@ -254,7 +254,7 @@ class Script(scripts.Script):
                 saved_original_selfattn_forward = org_attn_module.forward
                 org_attn_module.forward = xattn_forward_log.__get__(org_attn_module,org_attn_module.__class__)
                 current_sag_block_index = 0
-            elif current_unet_kwargs['sigma'] < current_max_sigma / 4:
+            elif current_unet_kwargs['sigma'] < current_max_sigma / 5: #4:
                 if current_sag_block_index == 1:
                     attn_module = shared.sd_model.model.diffusion_model.output_blocks[5]._modules['1'].transformer_blocks._modules['0'].attn1
                     attn_module.forward = saved_original_selfattn_forward
@@ -262,7 +262,7 @@ class Script(scripts.Script):
                     saved_original_selfattn_forward = org_attn_module.forward
                     org_attn_module.forward = xattn_forward_log.__get__(org_attn_module,org_attn_module.__class__)
                     current_sag_block_index = 2
-            elif current_unet_kwargs['sigma'] < current_max_sigma / 2:
+            elif current_unet_kwargs['sigma'] < current_max_sigma / 2.5: #2:
                 if current_sag_block_index == 0:
                     attn_module = shared.sd_model.model.diffusion_model.middle_block._modules['1'].transformer_blocks._modules['0'].attn1
                     attn_module.forward = saved_original_selfattn_forward

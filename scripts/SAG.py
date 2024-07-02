63b2702ffdc4877f04b561c08995ad1a8816e128
AG-w <44581470+AG-w@users.noreply.github.com>
2024-07-02 13:26:20 +0800

    Update SAG.py

--- scripts/SAG.py
+++ scripts/SAG.py
@@ -36,10 +36,11 @@ def adaptive_gaussian_blur_2d(img, sigma, kernel_size=None):
         kernel_size = kernel_size + 1 if kernel_size % 2 == 0 else kernel_size
 
     ksize_half = (kernel_size - 1) * 0.5
-    x = torch.linspace(-ksize_half, ksize_half, steps=kernel_size, device=img.device)
+    x = torch.linspace(-ksize_half, ksize_half, steps=kernel_size)
     pdf = torch.exp(-0.5 * (x / sigma).pow(2))
     x_kernel = pdf / pdf.sum()
-
+    x_kernel = x_kernel.to(device=img.device, dtype=img.dtype)
+    
     kernel2d = torch.mm(x_kernel[:, None], x_kernel[None, :])
     kernel2d = kernel2d.expand(img.shape[-3], 1, kernel2d.shape[0], kernel2d.shape[1])
 
